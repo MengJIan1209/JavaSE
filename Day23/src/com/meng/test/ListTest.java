a9d2d344be70cd6a7dc7ca01433ad5fbab35984d
MengJIan1209 <1936549016@qq.com>
2021-12-19 17:55:24 +0800

    练习的代码

---
+++ Day23/src/com/meng/test/ListTest.java
@@ -0,0 +1,27 @@
+package com.meng.test;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ */
+public class ListTest {
+
+    @Test
+    public void testListRemove() {
+        List list = new ArrayList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        updateList(list);
+        System.out.println(list);//
+    }
+    private static void updateList(List list) {
+//        list.remove(2);
+        list.remove(new Integer(2));
+    }
+
+}
