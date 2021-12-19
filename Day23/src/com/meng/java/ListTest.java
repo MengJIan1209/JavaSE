37bc817cdcdac7dea56f4ec4fe93f34651d44d92
MengJIan1209 <1936549016@qq.com>
2021-12-19 17:51:31 +0800

    练习的代码

--- Day23/src/com/meng/java/ListTest.java
+++ Day23/src/com/meng/java/ListTest.java
@@ -1,5 +1,11 @@
 package com.meng.java;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**1.List接口框架
  *  |----Collection接口：单列集合，用来存储一个一个的对象
  *               |----list接口：存储有序的可重复的接口，   -->动态 “数组” ，替换原有的数据
@@ -55,7 +61,115 @@ package com.meng.java;
  *   不同点：见上
  *
  *
+ *
+ * void add(int index, Object ele):在index位置插入ele元素
+ * boolean addAll(int index, Collection eles):从index位置开始将eles中的所有元素添加进来
+ * Object get(int index):获取指定index位置的元素
+ * int indexOf(Object obj):返回obj在集合中首次出现的位置
+ * int lastIndexOf(Object obj):返回obj在当前集合中末次出现的位置
+ * Object remove(int index):移除指定index位置的元素，并返回此元素
+ * Object set(int index, Object ele):设置指定index位置的元素为ele
+ * List subList(int fromIndex, int toIndex):返回从fromIndex到toIndex位置的子集合
+ *
+ *      总结：常用方法：
+ *           增：add(Object obj)
+ *           删：remove(int index)/remove(Object obj)
+ *           改：set(int index，Object ele)
+ *           查：get(int index)
+ *           插：add(int index,Object ele)
+ *           长度：size()
+ *           遍历：①：iterator迭代器方式
+ *                ②：增强for循环
+ *                ③：普通循环
+ *
  */
 public class ListTest {
+
+    @Test
+    public void test3(){
+        ArrayList list = new ArrayList();
+        list.add(123);
+        list.add(456);
+        list.add("AA");
+
+//        //①：iterator迭代器方式
+//        Iterator iterator = list.iterator();
+//        while (iterator.hasNext()){
+//            System.out.println(iterator.next());
+//        }
+
+//        // ②：增强for循环
+//        for(Object obj : list){
+//            System.out.println(obj);
+//        }
+
+        //③：普通循环
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+
+    }
+
+
+    @Test
+    public void test2(){
+
+        //int indexOf(Object obj):返回obj在集合中首次出现的位置:如果不存在返回-1
+        ArrayList list = new ArrayList();
+        list.add(123);
+        list.add(456);
+        list.add("AA");
+        list.add(new Person("Tom",12));
+        list.add(456);
+
+        int index = list.indexOf(4567);
+        System.out.println(index);
+
+        //int lastIndexOf(Object obj):返回obj在当前集合中末次出现的位置:如果不存在返回-1
+        System.out.println(list.lastIndexOf(456));
+
+        //Object remove(int index):移除指定index位置的元素，并返回此元素
+        Object obj = list.remove(0);
+        System.out.println(obj);
+        System.out.println(list);
+
+        //Object set(int index, Object ele):设置指定index位置的元素为ele
+        list.set(1,"CC");
+        System.out.println(list);
+
+        // List subList(int fromIndex, int toIndex):返回从fromIndex到toIndex位置左闭右开区间的子集合
+        List subList = list.subList(2, 4);
+        System.out.println(subList);
+        System.out.println(list);
+
+    }
+
+
+    @Test
+    public void test1(){
+        ArrayList list = new ArrayList();
+        list.add(123);
+        list.add(456);
+        list.add("AA");
+        list.add(new Person("Tom",12));
+        list.add(456);
+
+        //void add(int index, Object ele):在index位置插入ele元素
+        list.add(1,"BB");
+        System.out.println(list);
+
+        //boolean addAll(int index, Collection eles):从index位置开始将eles中的所有元素添加进来
+        List list1 = Arrays.asList(1, 2, 3);
+
+        list.addAll(list1);
+        System.out.println(list.size()); //9
+
+        //Object get(int index):获取指定index位置的元素
+        System.out.println(list.get(0));
+
+
+
+    }
 
 }
