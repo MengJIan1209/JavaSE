694900559e00767644a008eb63024e71fa09036a
MengJIan1209 <1936549016@qq.com>
2021-12-21 22:12:43 +0800

    练习的代码

--- Day23/src/com/meng/Set/User.java
+++ Day23/src/com/meng/Set/User.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 /**
  *
  */
-public class User {
+public class User implements Comparable{
 
     private  String name;
     private  int age;
@@ -55,5 +55,23 @@ public class User {
     @Override
     public int hashCode() {
         return Objects.hash(name, age);
+    }
+
+
+    @Override
+    public int compareTo(Object o) {
+        if (o instanceof  User){
+            User user = (User) o;
+//            return this.name.compareTo(user.name);
+            int compare = this.name.compareTo(user.name);
+            if (compare != 0){
+                return compare;
+            }else{
+                return Integer.compare(this.age,user.age);
+            }
+        }else{
+            throw new RuntimeException("输入的类型不匹配");
+        }
+
     }
 }
