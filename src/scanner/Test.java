654fe5f01ffd3cedfc6b109d9ca0f7bc2e38c9ea
mengjian1209 <1936549016@qq.com>
2022-02-25 17:53:22 +0800

    练习的代码

---
+++ src/scanner/Test.java
@@ -0,0 +1,16 @@
+package scanner;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        int a = 100,b=50;
+        int c = a---b,d=a---b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+
+}
